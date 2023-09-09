ffeaa1ff0de791215da2969f6e4c0a389395c3d9
swwind <i@sww.moe>
2023-09-09 16:19:48 +0800

    add preact-render-to-string

--- server-deps.ts
+++ server-deps.ts
@@ -7,7 +7,7 @@ export {
 } from "https://deno.land/std@0.201.0/path/mod.ts";
 export { typeByExtension } from "https://deno.land/std@0.201.0/media_types/mod.ts";
 
-export { render as renderToString } from "https://esm.sh/preact-render-to-string@6.2.1";
+export { render as renderToString } from "preact-render-to-string";
 
 export * as esbuild from "https://deno.land/x/esbuild@v0.19.2/mod.js";
 export { denoPlugins } from "https://deno.land/x/esbuild_deno_loader@0.8.1/mod.ts";
